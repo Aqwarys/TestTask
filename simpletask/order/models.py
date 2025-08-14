19118f1bcf77366f34a6c544cb2cc54785f2c40d
Alisher Aqwa <aqwaryss@gmail.com>
2025-08-15 00:14:22 +0500

    order model created and migrated

--- simpletask/order/models.py
+++ simpletask/order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Order(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False, verbose_name='Order Name')
+    description = models.TextField(blank=True, null=True, verbose_name='Description')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders', verbose_name='User')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
