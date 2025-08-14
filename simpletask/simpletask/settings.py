10abb094975dc5886019eb6689033eff584215e2
Alisher Aqwa <aqwaryss@gmail.com>
2025-08-14 23:11:19 +0500

    apps created and seted up configured

--- simpletask/simpletask/settings.py
+++ simpletask/simpletask/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd-party
+    'rest_framework',
+
+    # apps
+    'user.apps.UserConfig',
+    'order.apps.OrderConfig',
 ]
 
 MIDDLEWARE = [
